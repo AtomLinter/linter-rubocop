f4882beaf39f37966b8369b98210bcc318c5ee53
Max Schwenk <maschwenk@gmail.com>
2017-03-05 22:51:33 -0800

    Allow lint-on-fly to be configurable

--- src/index.js
+++ src/index.js
@@ -91,6 +91,11 @@ export default {
         this.disableWhenNoConfigFile = value;
       }),
     );
+    this.subscriptions.add(
+      atom.config.observe('linter-rubocop.lintOnFly', (value) => {
+        this.lintOnFly = value;
+      }),
+    );
   },
 
   deactivate() {
@@ -107,7 +112,7 @@ export default {
         'source.ruby.chef',
       ],
       scope: 'file',
-      lintOnFly: true,
+      lintOnFly: this.lintOnFly,
       lint: async (editor) => {
         const filePath = editor.getPath();
 
