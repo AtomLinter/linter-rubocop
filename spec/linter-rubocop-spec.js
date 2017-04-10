ab07dcc4d77391581b15341937072c1855382b89
Max Schwenk <maschwenk@gmail.com>
2017-04-09 18:35:45 -0700

    Not a fan of semicolons

--- spec/linter-rubocop-spec.js
+++ spec/linter-rubocop-spec.js
@@ -1,97 +1,97 @@
-'use babel';
+'use babel'
 
-import * as path from 'path';
-import { truncateSync, writeFileSync, readFileSync } from 'fs';
+import * as path from 'path'
+import { truncateSync, writeFileSync, readFileSync } from 'fs'
 // eslint-disable-next-line import/no-extraneous-dependencies
-import tmp from 'tmp';
+import tmp from 'tmp'
 
-const lint = require('../src/index.js').provideLinter().lint;
+const lint = require('../src/index.js').provideLinter().lint
 
-const badPath = path.join(__dirname, 'fixtures', 'lintableFiles', 'bad.rb');
-const emptyPath = path.join(__dirname, 'fixtures', 'lintableFiles', 'empty.rb');
-const goodPath = path.join(__dirname, 'fixtures', 'lintableFiles', 'good.rb');
-const invalidWithUrlPath = path.join(__dirname, 'fixtures', 'lintableFiles', 'invalid_with_url.rb');
-const ruby23Path = path.join(__dirname, 'fixtures', 'lintableFiles', 'ruby_2_3.rb');
-const ruby23PathYml22 = path.join(__dirname, 'fixtures', 'yml2_2', 'ruby_2_3.rb');
+const badPath = path.join(__dirname, 'fixtures', 'lintableFiles', 'bad.rb')
+const emptyPath = path.join(__dirname, 'fixtures', 'lintableFiles', 'empty.rb')
+const goodPath = path.join(__dirname, 'fixtures', 'lintableFiles', 'good.rb')
+const invalidWithUrlPath = path.join(__dirname, 'fixtures', 'lintableFiles', 'invalid_with_url.rb')
+const ruby23Path = path.join(__dirname, 'fixtures', 'lintableFiles', 'ruby_2_3.rb')
+const ruby23PathYml22 = path.join(__dirname, 'fixtures', 'yml2_2', 'ruby_2_3.rb')
 
 describe('The RuboCop provider for Linter', () => {
   beforeEach(() => {
     // Reset/set project path to fixtures
-    atom.project.setPaths([path.join(__dirname, 'fixtures')]);
+    atom.project.setPaths([path.join(__dirname, 'fixtures')])
 
-    atom.workspace.destroyActivePaneItem();
+    atom.workspace.destroyActivePaneItem()
 
     // Info about this beforeEach() implementation:
     // https://github.com/AtomLinter/Meta/issues/15
     const activationPromise =
-      atom.packages.activatePackage('linter-rubocop');
+      atom.packages.activatePackage('linter-rubocop')
 
     waitsForPromise(() =>
       atom.packages.activatePackage('language-ruby').then(() =>
         atom.workspace.open(goodPath),
-    ));
+    ))
 
-    atom.packages.triggerDeferredActivationHooks();
-    waitsForPromise(() => activationPromise);
-  });
+    atom.packages.triggerDeferredActivationHooks()
+    waitsForPromise(() => activationPromise)
+  })
 
   it('should be in the packages list', () =>
     expect(atom.packages.isPackageLoaded('linter-rubocop')).toBe(true),
-  );
+  )
 
   it('should be an active package', () =>
     expect(atom.packages.isPackageActive('linter-rubocop')).toBe(true),
-  );
+  )
 
   describe('shows errors in a file with errors', () => {
-    let editor = null;
+    let editor = null
 
     beforeEach(() => {
       waitsForPromise(() =>
-        atom.workspace.open(badPath).then((openEditor) => { editor = openEditor; }),
-      );
-    });
+        atom.workspace.open(badPath).then((openEditor) => { editor = openEditor }),
+      )
+    })
 
     it('verifies the first message', () => {
       const msgText = 'unterminated string meets end of file\n(Using Ruby 2.3 parser; ' +
-        'configure using `TargetRubyVersion` parameter, under `AllCops`) (Syntax)';
+        'configure using `TargetRubyVersion` parameter, under `AllCops`) (Syntax)'
 
       waitsForPromise(() =>
         lint(editor).then((messages) => {
-          expect(messages[0].severity).toBe('error');
-          expect(messages[0].excerpt).toBe(msgText);
-          expect(messages[0].description).toBe(null);
-          expect(messages[0].location.file).toBe(badPath);
-          expect(messages[0].location.position).toEqual([[1, 6], [1, 7]]);
+          expect(messages[0].severity).toBe('error')
+          expect(messages[0].excerpt).toBe(msgText)
+          expect(messages[0].description).toBe(null)
+          expect(messages[0].location.file).toBe(badPath)
+          expect(messages[0].location.position).toEqual([[1, 6], [1, 7]])
         }),
-      );
-    });
-  });
+      )
+    })
+  })
 
   describe('shows errors with a clickable link in a file with warnings', () => {
-    let editor = null;
+    let editor = null
 
     beforeEach(() => {
       waitsForPromise(() =>
-        atom.workspace.open(invalidWithUrlPath).then((openEditor) => { editor = openEditor; }),
-      );
-    });
+        atom.workspace.open(invalidWithUrlPath).then((openEditor) => { editor = openEditor }),
+      )
+    })
 
     it('verifies the first message', () => {
       const msgText = "Prefer single-quoted strings when you don't need " +
-        'string interpolation or special symbols. (Style/StringLiterals)';
+        'string interpolation or special symbols. (Style/StringLiterals)'
 
       waitsForPromise(() =>
         lint(editor).then((messages) => {
-          expect(messages[0].severity).toBe('info');
-          expect(messages[0].excerpt).toBe(msgText);
-          expect(messages[0].location.file).toBe(invalidWithUrlPath);
-          expect(messages[0].location.position).toEqual([[2, 6], [2, 20]]);
-          waitsForPromise(() => messages[0].description().then(desc => expect(desc).toBeTruthy()));
+          expect(messages[0].severity).toBe('info')
+          expect(messages[0].excerpt).toBe(msgText)
+          expect(messages[0].location.file).toBe(invalidWithUrlPath)
+          expect(messages[0].location.position).toEqual([[2, 6], [2, 20]])
+          waitsForPromise(() => messages[0].description().then(desc => expect(desc).toBeTruthy()))
         }),
-      );
-    });
-  });
+      )
+    })
+  })
 
   it('finds nothing wrong with an empty file', () => {
     waitsForPromise(() =>
@@ -100,8 +100,8 @@ describe('The RuboCop provider for Linter', () => {
           expect(messages.length).toBe(0),
         ),
       ),
-    );
-  });
+    )
+  })
 
   it('finds nothing wrong with a valid file', () => {
     waitsForPromise(() =>
@@ -110,20 +110,20 @@ describe('The RuboCop provider for Linter', () => {
           expect(messages.length).toBe(0),
         ),
       ),
-    );
-  });
+    )
+  })
 
   describe('respects .ruby-version when .rubycop.yml has not defined ruby version', () => {
     it('finds violations when .rubocop.yml sets syntax to Ruby 2.2', () => {
-      atom.project.setPaths([path.join(__dirname, 'fixtures', 'yml2_2')]);
+      atom.project.setPaths([path.join(__dirname, 'fixtures', 'yml2_2')])
       waitsForPromise(() =>
         atom.workspace.open(ruby23PathYml22).then(editor =>
           lint(editor).then(messages =>
             expect(messages.length).toBe(1),
           ),
         ),
-      );
-    });
+      )
+    })
 
     it('finds nothing wrong with a file when .rubocop.yml does not override the Ruby version', () => {
       waitsForPromise(() =>
@@ -132,54 +132,54 @@ describe('The RuboCop provider for Linter', () => {
             expect(messages.length).toBe(0),
           ),
         ),
-      );
-    });
-  });
+      )
+    })
+  })
 
   describe('allows the user to autocorrect the current file', () => {
-    let doneCorrecting;
-    const tmpobj = tmp.fileSync({ postfix: '.rb' });
+    let doneCorrecting
+    const tmpobj = tmp.fileSync({ postfix: '.rb' })
     const checkNotificaton = (notification) => {
-      const message = notification.getMessage();
+      const message = notification.getMessage()
       if (message === 'Linter-Rubocop: No fixes were made') {
-        expect(notification.getType()).toBe('info');
+        expect(notification.getType()).toBe('info')
       } else {
-        expect(message).toMatch(/Linter-Rubocop: Fixed \d offenses/);
-        expect(notification.getType()).toBe('success');
+        expect(message).toMatch(/Linter-Rubocop: Fixed \d offenses/)
+        expect(notification.getType()).toBe('success')
       }
-      doneCorrecting = true;
-    };
+      doneCorrecting = true
+    }
 
     beforeEach(() => {
-      truncateSync(tmpobj.name);
-      doneCorrecting = false;
-    });
+      truncateSync(tmpobj.name)
+      doneCorrecting = false
+    })
 
     it('corrects the bad file', () => {
-      writeFileSync(tmpobj.name, readFileSync(invalidWithUrlPath));
+      writeFileSync(tmpobj.name, readFileSync(invalidWithUrlPath))
       waitsForPromise(() =>
         atom.workspace.open(tmpobj.name).then((editor) => {
-          atom.notifications.onDidAddNotification(checkNotificaton);
-          atom.commands.dispatch(atom.views.getView(editor), 'linter-rubocop:fix-file');
+          atom.notifications.onDidAddNotification(checkNotificaton)
+          atom.commands.dispatch(atom.views.getView(editor), 'linter-rubocop:fix-file')
         }),
-      );
+      )
       waitsFor(
         () => doneCorrecting,
         'Notification type should be checked',
-      );
-    });
+      )
+    })
 
     it("doesn't modify a good file", () => {
       waitsForPromise(() =>
         atom.workspace.open(goodPath).then((editor) => {
-          atom.notifications.onDidAddNotification(checkNotificaton);
-          atom.commands.dispatch(atom.views.getView(editor), 'linter-rubocop:fix-file');
+          atom.notifications.onDidAddNotification(checkNotificaton)
+          atom.commands.dispatch(atom.views.getView(editor), 'linter-rubocop:fix-file')
         }),
-      );
+      )
       waitsFor(
         () => doneCorrecting,
         'Notification type should be checked',
-      );
-    });
-  });
-});
+      )
+    })
+  })
+})
--- src/index.js
+++ src/index.js
@@ -1,99 +1,99 @@
-'use babel';
+'use babel'
 
 // eslint-disable-next-line import/extensions, import/no-extraneous-dependencies
-import { CompositeDisposable } from 'atom';
-import path from 'path';
-import pluralize from 'pluralize';
-import * as helpers from 'atom-linter';
-import { get } from 'request-promise';
+import { CompositeDisposable } from 'atom'
+import path from 'path'
+import pluralize from 'pluralize'
+import * as helpers from 'atom-linter'
+import { get } from 'request-promise'
 
 const DEFAULT_ARGS = [
   '--cache', 'false',
   '--force-exclusion',
   '--format', 'json',
   '--display-style-guide',
-];
-const DOCUMENTATION_LIFETIME = 86400 * 1000; // 1 day TODO: Configurable?
+]
+const DOCUMENTATION_LIFETIME = 86400 * 1000 // 1 day TODO: Configurable?
 
-const docsRuleCache = new Map();
-let docsLastRetrieved;
+const docsRuleCache = new Map()
+let docsLastRetrieved
 
 const takeWhile = (source, predicate) => {
-  const result = [];
-  const length = source.length;
-  let i = 0;
+  const result = []
+  const length = source.length
+  let i = 0
 
   while (i < length && predicate(source[i], i)) {
-    result.push(source[i]);
-    i += 1;
+    result.push(source[i])
+    i += 1
   }
 
-  return result;
-};
+  return result
+}
 
 const parseFromStd = (stdout, stderr) => {
-  let parsed;
+  let parsed
   try {
-    parsed = JSON.parse(stdout);
+    parsed = JSON.parse(stdout)
   } catch (error) {
     // continue regardless of error
   }
-  if (typeof parsed !== 'object') { throw new Error(stderr || stdout); }
-  return parsed;
-};
+  if (typeof parsed !== 'object') { throw new Error(stderr || stdout) }
+  return parsed
+}
 
 const getProjectDirectory = filePath =>
-                              atom.project.relativizePath(filePath)[0] || path.dirname(filePath);
+                              atom.project.relativizePath(filePath)[0] || path.dirname(filePath)
 
 
 // Retrieves style guide documentation with cached responses
 const getMarkDown = async (url) => {
-  const anchor = url.split('#')[1];
+  const anchor = url.split('#')[1]
 
   if (new Date().getTime() - docsLastRetrieved < DOCUMENTATION_LIFETIME) {
     // If documentation is stale, clear cache
-    docsRuleCache.clear();
+    docsRuleCache.clear()
   }
 
-  if (docsRuleCache.has(anchor)) { return docsRuleCache.get(anchor); }
+  if (docsRuleCache.has(anchor)) { return docsRuleCache.get(anchor) }
 
-  let rawRulesMarkdown;
+  let rawRulesMarkdown
   try {
-    rawRulesMarkdown = await get('https://raw.githubusercontent.com/bbatsov/ruby-style-guide/master/README.md');
+    rawRulesMarkdown = await get('https://raw.githubusercontent.com/bbatsov/ruby-style-guide/master/README.md')
   } catch (x) {
-    return '***\nError retrieving documentation';
+    return '***\nError retrieving documentation'
   }
 
-  const byLine = rawRulesMarkdown.split('\n');
+  const byLine = rawRulesMarkdown.split('\n')
   // eslint-disable-next-line no-confusing-arrow
   const ruleAnchors = byLine.reduce((acc, line, idx) =>
                                       line.match(/\* <a name=/g) ? acc.concat([[idx, line]]) : acc,
-                                      []);
+                                      [])
 
   ruleAnchors.forEach(([startingIndex, startingLine]) => {
-    const ruleName = startingLine.split('"')[1];
-    const beginSearch = byLine.slice(startingIndex + 1);
+    const ruleName = startingLine.split('"')[1]
+    const beginSearch = byLine.slice(startingIndex + 1)
 
     // gobble all the documentation until you reach the next rule
-    const documentationForRule = takeWhile(beginSearch, x => !x.match(/\* <a name=/));
-    const markdownOutput = '***\n'.concat(documentationForRule.join('\n'));
+    const documentationForRule = takeWhile(beginSearch, x => !x.match(/\* <a name=/))
+    const markdownOutput = '***\n'.concat(documentationForRule.join('\n'))
 
-    docsRuleCache.set(ruleName, markdownOutput);
-  });
+    docsRuleCache.set(ruleName, markdownOutput)
+  })
 
-  docsLastRetrieved = new Date().getTime();
-  return docsRuleCache.get(anchor);
-};
+  docsLastRetrieved = new Date().getTime()
+  return docsRuleCache.get(anchor)
+}
 
 const forwardRubocopToLinter =
   ({ message: rawMessage, location, severity, cop_name: copName }, file, editor) => {
-    const [excerpt, url] = rawMessage.split(/ \((.*)\)/, 2);
-    let position;
+    const [excerpt, url] = rawMessage.split(/ \((.*)\)/, 2)
+    let position
     if (location) {
-      const { line, column, length } = location;
-      position = [[line - 1, column - 1], [line - 1, (column + length) - 1]];
+      const { line, column, length } = location
+      position = [[line - 1, column - 1], [line - 1, (column + length) - 1]]
     } else {
-      position = helpers.generateRange(editor, 0);
+      position = helpers.generateRange(editor, 0)
     }
 
     const severityMapping = {
@@ -102,7 +102,7 @@ const forwardRubocopToLinter =
       warning: 'warning',
       error: 'error',
       fatal: 'error',
-    };
+    }
 
     const linterMessage = {
       url,
@@ -113,57 +113,57 @@ const forwardRubocopToLinter =
         file,
         position,
       },
-    };
-    return linterMessage;
-  };
+    }
+    return linterMessage
+  }
 
 export default {
   activate() {
-    require('atom-package-deps').install('linter-rubocop', true);
+    require('atom-package-deps').install('linter-rubocop', true)
 
-    this.subscriptions = new CompositeDisposable();
+    this.subscriptions = new CompositeDisposable()
 
     // Register fix command
     this.subscriptions.add(
       atom.commands.add('atom-text-editor', {
         'linter-rubocop:fix-file': async () => {
-          const textEditor = atom.workspace.getActiveTextEditor();
+          const textEditor = atom.workspace.getActiveTextEditor()
 
           if (!atom.workspace.isTextEditor(textEditor) || textEditor.isModified()) {
             // Abort for invalid or unsaved text editors
-            return atom.notifications.addError('Linter-Rubocop: Please save before fixing');
+            return atom.notifications.addError('Linter-Rubocop: Please save before fixing')
           }
 
-          const filePath = textEditor.getPath();
+          const filePath = textEditor.getPath()
           const command = this.command
                               .split(/\s+/)
                               .filter(i => i)
-                              .concat(DEFAULT_ARGS, '--auto-correct', filePath);
-          const cwd = getProjectDirectory(filePath);
-          const { stdout, stderr } = await helpers.exec(command[0], command.slice(1), { cwd, stream: 'both' });
-          const { summary: { offense_count: offenseCount } } = parseFromStd(stdout, stderr);
+                              .concat(DEFAULT_ARGS, '--auto-correct', filePath)
+          const cwd = getProjectDirectory(filePath)
+          const { stdout, stderr } = await helpers.exec(command[0], command.slice(1), { cwd, stream: 'both' })
+          const { summary: { offense_count: offenseCount } } = parseFromStd(stdout, stderr)
           return offenseCount === 0 ?
             atom.notifications.addInfo('Linter-Rubocop: No fixes were made') :
-            atom.notifications.addSuccess(`Linter-Rubocop: Fixed ${pluralize('offenses', offenseCount, true)}`);
+            atom.notifications.addSuccess(`Linter-Rubocop: Fixed ${pluralize('offenses', offenseCount, true)}`)
         },
       }),
-    );
+    )
 
     // Config observers
     this.subscriptions.add(
       atom.config.observe('linter-rubocop.command', (value) => {
-        this.command = value;
+        this.command = value
       }),
-    );
+    )
     this.subscriptions.add(
       atom.config.observe('linter-rubocop.disableWhenNoConfigFile', (value) => {
-        this.disableWhenNoConfigFile = value;
+        this.disableWhenNoConfigFile = value
       }),
-    );
+    )
   },
 
   deactivate() {
-    this.subscriptions.dispose();
+    this.subscriptions.dispose()
   },
 
   provideLinter() {
@@ -178,26 +178,26 @@ export default {
       scope: 'file',
       lintsOnChange: true,
       lint: async (editor) => {
-        const filePath = editor.getPath();
+        const filePath = editor.getPath()
 
         if (this.disableWhenNoConfigFile === true) {
-          const config = await helpers.findAsync(filePath, '.rubocop.yml');
+          const config = await helpers.findAsync(filePath, '.rubocop.yml')
           if (config === null) {
-            return [];
+            return []
           }
         }
 
         const command = this.command
                             .split(/\s+/)
                             .filter(i => i)
-                            .concat(DEFAULT_ARGS, '--stdin', filePath);
-        const stdin = editor.getText();
-        const cwd = getProjectDirectory(filePath);
-        const { stdout, stderr } = await helpers.exec(command[0], command.slice(1), { cwd, stdin, stream: 'both' });
-        const { files } = parseFromStd(stdout, stderr);
-        const offenses = files && files[0] && files[0].offenses;
-        return (offenses || []).map(offense => forwardRubocopToLinter(offense, filePath, editor));
+                            .concat(DEFAULT_ARGS, '--stdin', filePath)
+        const stdin = editor.getText()
+        const cwd = getProjectDirectory(filePath)
+        const { stdout, stderr } = await helpers.exec(command[0], command.slice(1), { cwd, stdin, stream: 'both' })
+        const { files } = parseFromStd(stdout, stderr)
+        const offenses = files && files[0] && files[0].offenses
+        return (offenses || []).map(offense => forwardRubocopToLinter(offense, filePath, editor))
       },
-    };
+    }
   },
-};
+}
