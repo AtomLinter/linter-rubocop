d916f907846f2549b34bf9da4d202205426fa522
Víctor Zamanillo <bellzebu@hotmail.com>
2019-06-27 09:46:21 +0200

    refactor: add subscriptions, Rubocop main initialization on activate
    - Using this.subscriptions.add per subscription clarifies the code a lot.
    - We initialize the Rubocop main class on activate.

--- src/index.js
+++ src/index.js
@@ -29,53 +29,58 @@ export default {
     this.idleCallbacks.add(depsCallbackID)
 
     this.subscriptions = new CompositeDisposable()
-    this.subscriptions.add(
-      // Register autocorrect command
-      atom.commands.add('atom-text-editor', {
-        'linter-rubocop:fix-file': async () => {
-          const editor = atom.workspace.getActiveTextEditor()
-          if (hasValidScope(editor, this.scopes)) {
-            await this.fixFile(editor)
+
+    // Register autocorrect command
+    this.subscriptions.add(atom.commands.add('atom-text-editor', {
+      'linter-rubocop:fix-file': async () => {
+        const editor = atom.workspace.getActiveTextEditor()
+        if (hasValidScope(editor, this.scopes)) {
+          await this.fixFile(editor)
+        }
+      },
+    }))
+
+    this.subscriptions.add(atom.contextMenu.add({
+      'atom-text-editor:not(.mini), .overlayer': [{
+        label: 'Fix file with Rubocop',
+        command: 'linter-rubocop:fix-file',
+        shouldDisplay: (evt) => {
+          const activeEditor = atom.workspace.getActiveTextEditor()
+          if (!activeEditor) {
+            return false
           }
+          // Black magic!
+          // Compares the private component property of the active TextEditor
+          //   against the components of the elements
+          const evtIsActiveEditor = evt.path.some(elem => (
+            // Atom v1.19.0+
+            elem.component && activeEditor.component
+              && elem.component === activeEditor.component))
+          // Only show if it was the active editor and it is a valid scope
+          return evtIsActiveEditor && hasValidScope(activeEditor, this.scopes)
         },
-      }),
-      atom.contextMenu.add({
-        'atom-text-editor:not(.mini), .overlayer': [{
-          label: 'Fix file with Rubocop',
-          command: 'linter-rubocop:fix-file',
-          shouldDisplay: (evt) => {
-            const activeEditor = atom.workspace.getActiveTextEditor()
-            if (!activeEditor) {
-              return false
-            }
-            // Black magic!
-            // Compares the private component property of the active TextEditor
-            //   against the components of the elements
-            const evtIsActiveEditor = evt.path.some(elem => (
-              // Atom v1.19.0+
-              elem.component && activeEditor.component
-                && elem.component === activeEditor.component))
-            // Only show if it was the active editor and it is a valid scope
-            return evtIsActiveEditor && hasValidScope(activeEditor, this.scopes)
-          },
-        }],
-      }),
-      atom.config.observe('linter-rubocop.command', (value) => {
-        this.command = value
-      }),
-      atom.config.observe('linter-rubocop.disableWhenNoConfigFile', (value) => {
-        this.disableWhenNoConfigFile = value
-      }),
-      atom.config.observe('linter-rubocop.useBundler', (value) => {
-        this.useBundler = value
+      }],
+    }))
+
+    this.subscriptions.add(atom.config.observe('linter-rubocop.command', (value) => {
+      this.command = value
+    }))
+
+    this.subscriptions.add(atom.config.observe('linter-rubocop.disableWhenNoConfigFile', (value) => {
+      this.disableWhenNoConfigFile = value
+    }))
+
+    this.subscriptions.add(atom.config.observe('linter-rubocop.useBundler', (value) => {
+      this.useBundler = value
+    }))
+
+    this.rubocop = new Rubocop(
+      new RubocopConfig({
+        command: this.command,
+        disableWhenNoConfigFile: this.disableWhenNoConfigFile,
+        useBundler: this.useBundler,
       }),
     )
-
-    this.rubocopConfig = new RubocopConfig({
-      command: this.command,
-      disableWhenNoConfigFile: this.disableWhenNoConfigFile,
-      useBundler: this.useBundler,
-    })
   },
 
   deactivate() {
@@ -98,7 +103,7 @@ export default {
       return
     }
 
-    new Rubocop(this.rubocopConfig).autocorrect(editor.getPath())
+    this.rubocop.autocorrect(editor.getPath())
   },
 
   provideLinter() {
@@ -111,8 +116,7 @@ export default {
         const filePath = editor.getPath()
         if (!filePath) { return null }
 
-        const messages = await new Rubocop(this.rubocopConfig)
-          .analyze(editor.getText(), filePath)
+        const messages = await this.rubocop.analyze(editor.getText(), filePath)
 
         return messages
       },
