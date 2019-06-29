37a4a578f8f5353ba19dfcb084699e8177d427fb
bellzebu <bellzebu@hotmail.com>
2019-06-29 22:51:58 +0200

    refactor: getting current directory
    we get current directory using path if relativizePath returns null, better variable and function names

--- src/rubocop/RubocopRunner.js
+++ src/rubocop/RubocopRunner.js
@@ -1,25 +1,26 @@
 'use babel'
 
+import path from 'path'
 import { exec, findAsync } from 'atom-linter'
 
-const CFG_FILE = '.rubocop.yml'
+const CONFIG_FILE = '.rubocop.yml'
 
 const TIMEOUT_ERROR_MSG = 'Process execution timed out'
-const LINTER_TIMEOUT_INFO_MSG = 'Linter-Rubocop: Linter timed out'
-const LINTER_TIMEOUT_INFO_DESC = 'Make sure you are not running Rubocop with a slow-starting interpreter like JRuby. '
-                                 + 'If you are still seeing timeouts, consider running your linter `on save` and not `on change`, '
-                                 + 'or reference https://github.com/AtomLinter/linter-rubocop/issues/202 .'
+const LINTER_TIMEOUT_MSG = 'Linter-Rubocop: Linter timed out'
+const LINTER_TIMEOUT_DESC = 'Make sure you are not running Rubocop with a slow-starting interpreter like JRuby. '
+                            + 'If you are still seeing timeouts, consider running your linter `on save` and not `on change`, '
+                            + 'or reference https://github.com/AtomLinter/linter-rubocop/issues/202 .'
 
 
-function buildBaseExecutionOpts(filePath, extraOptions = {}) {
-  const baseExecOptions = {
-    cwd: atom.project.relativizePath(filePath)[0],
+function buildExecOptions(filePath, extraOptions = {}) {
+  const baseOptions = {
+    cwd: atom.project.relativizePath(filePath)[0] || path.dirname(filePath),
     stream: 'both',
     timeout: 10000,
     uniqueKey: `linter-rubocop::${filePath}`,
     ignoreExitCode: true,
   }
-  return Object.assign(baseExecOptions, extraOptions)
+  return Object.assign(baseOptions, extraOptions)
 }
 
 export default class RubocopRunner {
@@ -29,25 +30,25 @@ export default class RubocopRunner {
 
   async run(filePath, args, options = {}) {
     if (this.config.disableWhenNoConfigFile === true) {
-      const configFile = await findAsync(filePath, CFG_FILE)
+      const configFile = await findAsync(filePath, CONFIG_FILE)
       if (configFile === null) {
         return null
       }
     }
 
-    const baseCommand = this.config.baseCommand.concat(args)
+    const command = this.config.baseCommand.concat(args)
     let output
     try {
       output = await exec(
-        baseCommand[0],
-        baseCommand.slice(1),
-        buildBaseExecutionOpts(filePath, options),
+        command[0],
+        command.slice(1),
+        buildExecOptions(filePath, options),
       )
     } catch (e) {
       if (e.message !== TIMEOUT_ERROR_MSG) {
         throw e
       }
-      atom.notifications.addInfo(LINTER_TIMEOUT_INFO_MSG, { description: LINTER_TIMEOUT_INFO_DESC })
+      atom.notifications.addInfo(LINTER_TIMEOUT_MSG, { description: LINTER_TIMEOUT_DESC })
       return null
     }
     return output
